d1b1785f018b517d2fb0f74dbf1c08c007bf5dd4
Heron Rossi <heron@squad.com.br>
2019-01-16 13:37:32 -0200

    Models revamp

--- api/models.py
+++ api/models.py
@@ -3,30 +3,35 @@ from django.db import models
 
 class Distrito(models.Model):
     nome = models.CharField(max_length=18)
+    cod_dist = models.CharField(max_length=9, unique=True)
 
 
 class SubPrefeitura(models.Model):
     nome = models.CharField(max_length=25)
+    cod_sub_pref = models.CharField(max_length=2, unique=True)
 
 
 class Regiao(models.Model):
-    nome = models.CharField(max_length=6)
+    nome = models.CharField(max_length=6, unique=True)
 
 
 class SubRegiao(models.Model):
     nome = models.CharField(max_length=7)
-    regiao_5 = models.ForeignKey(
+    regiao = models.ForeignKey(
         Regiao, related_name="sub_regioes", on_delete=models.CASCADE
     )
 
+    class Meta:
+        unique_together = ("nome", "regiao")
+
 
 class Bairro(models.Model):
-    nome = models.CharField(max_length=20)
+    nome = models.CharField(max_length=20, unique=True)
 
 
 class Feira(models.Model):
     nome = models.CharField(max_length=30)
-    registro = models.CharField(max_length=6)
+    registro = models.CharField(max_length=6, unique=True)
     long = models.CharField(max_length=10)
     lat = models.CharField(max_length=10)
     setcens = models.CharField(max_length=15)
@@ -41,4 +46,7 @@ class Feira(models.Model):
     sub_pref = models.ForeignKey(
         SubPrefeitura, related_name="feiras", on_delete=models.CASCADE
     )
-    regiao = models.ForeignKey(Regiao, related_name="feiras", on_delete=models.CASCADE)
+    sub_regiao = models.ForeignKey(
+        SubRegiao, related_name="feiras", on_delete=models.CASCADE
+    )
+    bairro = models.ForeignKey(Bairro, related_name="bairro", on_delete=models.CASCADE)
